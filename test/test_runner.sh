b4006417a7bf2bf3b1d819227dab598eea7e7f58
Andre Nathan <andre@digirati.com.br>
2017-03-03 15:01:16 -0300

    Better test runner output

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -5,9 +5,30 @@ run_test() {
   ./_build/ppx test/$name.ml > /dev/null
 }
 
-for i in test/*ml; do
-  test=`basename $i .ml`
-  run_test $test
+ok_or_error() {
+  out=`$@ 2>&1`
+  if [ $? -eq 0 ]; then
+    echo "OK"
+  else
+    echo "ERROR"
+    echo "-------------------------------------------------------------"
+    /bin/echo "$out"
+    echo "============================================================="
+  fi
+}
+
+rm -rf interop
+
+for t in test/*ml; do
+  name=`basename $t .ml`
+  echo -n "[-] Generating code for test '$name': "
+  ok_or_error run_test $name
 done
 
-diff -ruN test/expected interop
+for lang in `ls interop`; do
+  echo "[-] Checking $lang output"
+  for test in `ls test/expected/$lang`; do
+    echo -n "    * $test: "
+    ok_or_error diff -ru test/expected/$lang/$test interop/$lang/$test
+  done
+done
